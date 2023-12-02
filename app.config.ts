1d1f748d6f648464cd9cb6fcfa44aa6c9732c715
ludovik.allen <ludovik.allen@gsoft.com>
2023-12-01 21:22:32 -0500

    Change icon to use outline teeny icons

--- app.config.ts
+++ app.config.ts
@@ -1,45 +1,54 @@
 export default defineAppConfig({
   alpine: {
-    title: '',
-    description: '',
+    title: "",
+    description: "",
     image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      src: "/social-card-preview.png",
+      alt: "An image showcasing my project.",
       width: 400,
-      height: 300
+      height: 300,
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: "right", // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
-      }
+        path: "/logo.svg", // path of the logo
+        pathDark: "/logo-dark.svg", // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: "alpine", // alt of the logo
+      },
     },
     footer: {
       credits: {
         enabled: false, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        repository: "https://www.github.com/nuxt-themes/alpine", // our github repository
       },
       navigation: false, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
+      alignment: "center", // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: "", // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: '',
-      instagram: '',
+      twitter: "",
+      instagram: "",
 
       linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/company/nuxtlabs'
+        icon: "teenyicons:linkedin-outline",
+        label: "LinkedIn",
+        href: "https://www.linkedin.com/in/ludovik-allen-b7201714a/",
       },
 
-      github: 'ludovikallen',
-      medium: '@ludovikallen'
+      medium: {
+        icon: "teenyicons:medium-outline",
+        label: "Medium",
+        href: "https://www.medium.com/@ludovikallen",
+      },
+
+      github: {
+        icon: "teenyicons:github-outline",
+        label: "GitHub",
+        href: "https://github.com/ludovikallen",
+      },
     },
     form: {
-      successMessage: 'Message sent. Thank you!'
-    }
-  }
-})
+      successMessage: "Message sent. Thank you!",
+    },
+  },
+});
